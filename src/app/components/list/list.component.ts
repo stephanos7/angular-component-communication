9f3a2e9235278dab348605892be0d1cb17cae673
Stephanos Theodotou <stephanostheodotou@Stephanoss-MacBook-Pro.local>
2017-07-29 19:09:41 +0200

    add array of objects in list component

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -6,7 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-
+list : Object[] = [{
+                  id: 1,
+               quote: "Strive not to be a success, but rather to be of value.",
+              author: "Albert Einstein"
+},
+{
+                  id: 2,
+               quote: "I attribute my success to this: I never gave or took any excuse.",
+              author: "Florence Nightingale"
+},
+{
+                  id: 3,
+               quote: "You miss 100% of the shots you don’t take.",
+              author: "Wayne Gretzky"
+},
+{
+                  id: 4,
+               quote: "Life isn't about getting and having, it's about giving and being.",
+              author: "Kevin Kruse"
+},
+{
+                  id: 5,
+               quote: "Life is what happens to you while you’re busy making other plans.",
+              author: "John Lennon"
+},
+{
+                  id: 6,
+               quote: "You can never cross the ocean until you have the courage to lose sight of the shore. ",
+              author: "Christopher Columbus"
+},
+]
   constructor() { }
 
   ngOnInit() {
